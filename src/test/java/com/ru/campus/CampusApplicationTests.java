61c23977bfb188aca6b9107fd8db8b080e0ec47c
Atila Yumer <atillayumer@gmail.com>
2020-12-20 23:14:55 +0200

    First commit

---
+++ src/test/java/com/ru/campus/CampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ru.campus;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
